bfbc6dabba7b04ce91872bccd3d3ed96ae1745c3
MjhX <mjhandrock@gmail.com>
2025-06-22 10:49:28 -0500

    Small tweak to main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ from website import copy_contents, generate_page
 def main():
     copy_contents("static", "public")
     generate_page("content/index.md", "template.html", "public/index.html")
-    
 
 if __name__ == "__main__":
     main()
