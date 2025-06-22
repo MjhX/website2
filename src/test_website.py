1bb1aa2e4c0d8ab6e8fed4902bf8b0011de9b385
MjhX <mjhandrock@gmail.com>
2025-06-22 10:41:57 -0500

    Set up initial page generation structure

---
+++ src/test_website.py
@@ -0,0 +1,12 @@
+import unittest
+
+from website import extract_title
+
+
+class TestTextNode(unittest.TestCase):
+    pass
+
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/website.py
+++ src/website.py
@@ -15,3 +15,13 @@ def copy_contents(source, destination):
             shutil.copy(source_join,dest_join)
         else:
             raise Exception(f"What is {source_join}?")
+        
+def extract_title(markdown):
+    pass
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    # Read *.md from from_path
+    # Read *.html from template_path
+    # Use markdown_to_html_node and .to_html()
+    
