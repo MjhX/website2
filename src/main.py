4ce6d29046af189e7cad8bf6e51e9f8a5d9a1a6f
MjhX <mjhandrock@gmail.com>
2025-06-25 19:53:05 -0500

    Fixed the argv index

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from website import copy_contents, generate_pages_recursive
 import sys
 
 def main():
-    basepath = sys.argv[0] if len(sys.argv) > 0 else "/"
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
     copy_contents("static", "docs")
     generate_pages_recursive("content", "template.html", "docs", basepath)
 
